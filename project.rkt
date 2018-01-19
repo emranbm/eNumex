3268a183f0f1ea2366fa95769dc3a8921d004e8b
emran <emran.bm@gmail.com>
2018-01-19 16:37:48 +0330

    Mult and negation

--- project.rkt
+++ project.rkt
@@ -9,9 +9,13 @@
 ;; Add the missing ones
 
 (struct int  (num)    #:transparent)  ;; a constant number, e.g., (int 17)
-(struct var  (string) #:transparent)  ;; a string
-(struct apair (first second) #:transparent) 
+(struct bool (val) #:transparent) ;; a boolean, e.g, (bool #t)
+(struct var  (name) #:transparent)  ;; a variable
+(struct apair (first second) #:transparent)
+
 (struct add  (e1 e2)  #:transparent)  ;; add two expressions
+(struct mult  (e1 e2)  #:transparent)  ;; multiply two expressions
+(struct neg (e))
 
 
 (struct fun  (nameopt formal body) #:transparent) ;; a recursive(?) 1-argument function
@@ -29,14 +33,12 @@
   (cond [(var? r) r]
         [(int? r) r]
         [(number? r) (int r)]
-        [(string? r) (var r)]
         [#t "ERROR: Invalid type"]
          )
   )
 
 (define (numex-to-racket r)
-  (cond [(var? r) (var-string r)]
-        [(int? r) (int-num r)]
+  (cond [(int? r) (int-num r)]
         [(number? r) r]
         [(string? r) r]
         [#t "ERROR: Invalid type"]
@@ -61,3 +63,51 @@
         [#t (append (list (numex-to-racket (apair-first xs))) (numexlist->racketlist (apair-second xs)))]
    )
   )
+
+;; Problem 2
+
+;; lookup a variable in an environment
+;; Complete this function
+(define (envlookup env str)
+  (cond [(null? env) (error "unbound variable during evaluation" str)]
+        [(equal? (car (first env)) str) (cdr (first env))]
+        [#t (envlookup (rest env) str)]
+        )
+  )
+
+;; Do NOT change the two cases given to you.  
+;; DO add more cases for other kinds of NUMEX expressions.
+;; We will test eval-under-env by calling it directly even though
+;; "in real life" it would be a helper function of eval-exp.
+(define (eval-under-env e env)
+  (cond [(int? e) e]
+        [(bool? e) e]
+        [(var? e) 
+         (envlookup env (var-name e))]
+        [(add? e) 
+         (let ([v1 (eval-under-env (add-e1 e) env)]
+               [v2 (eval-under-env (add-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (int (+ (int-num v1) 
+                       (int-num v2)))
+               (error "NUMEX addition applied to non-number")))]
+        [(mult? e) 
+         (let ([v1 (eval-under-env (mult-e1 e) env)]
+               [v2 (eval-under-env (mult-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (int (* (int-num v1) 
+                       (int-num v2)))
+               (error "NUMEX multiplication applied to non-number")))]
+        [(neg? e)
+         (let ([v (eval-under-env (neg-e e) env)])
+           (cond [(int? v) (int (- (int-num v)))]
+                 [(bool? v) (bool (not (bool-val v)))]
+                 [#t (error "NUMEX negation is just for int and bool types")]
+           ))]
+        [#t (error (format "bad NUMEX expression: ~v" e))]))
+
+;; Do NOT change
+(define (eval-exp e)
+  (eval-under-env e null))
